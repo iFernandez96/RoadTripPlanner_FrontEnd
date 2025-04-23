fd25940da30cbe04619f2d813386e68781647cdb
albertogarcia <albergarcia@csumb.edu>
2025-04-23 02:07:51 -0700

    Configuration file changes

--- app.config.js
+++ app.config.js
@@ -1,23 +1,3 @@
-// const path = require('path');
-// const dotenv = require('dotenv');
-
-// const envPath = process.env.ENV_FILE
-//   ? path.resolve(process.env.ENV_FILE)
-//   : path.resolve('.env');
-
-// const env = dotenv.config({ path: envPath }).parsed || {};
-
-// export default {
-//   expo: {
-//     name: "RoadTripPlanner",
-//     extra: {
-//       apiUrl: env.API_URL,
-//       apiKey: env.API_KEY,
-//       environment: process.env.NODE_ENV || 'development',
-//     },
-//   },
-// };
-
 const path = require('path');
 const dotenv = require('dotenv');
 
