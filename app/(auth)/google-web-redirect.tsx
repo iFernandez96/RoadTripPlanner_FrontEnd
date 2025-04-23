64157597315e0cdc52915e8895b2b491961aec0b
albertogarcia <albergarcia@csumb.edu>
2025-04-23 15:26:00 -0700

    Added a redirect page for web to send the user to the Google Profile page.

---
+++ app/(auth)/google-web-redirect.tsx
@@ -0,0 +1,34 @@
+import { useEffect } from 'react';
+import { useRouter, useLocalSearchParams } from 'expo-router';
+import * as SecureStore from 'expo-secure-store';
+import { Alert, Platform } from 'react-native';
+
+export default function GoogleWebRedirect() {
+  const router = useRouter();
+  const { token } = useLocalSearchParams();
+
+  const storeToken = async (token: string) => {
+    if (Platform.OS === 'web') {
+      localStorage.setItem('userToken', token);
+    } else {
+      await SecureStore.setItemAsync('userToken', token);
+    }
+  };
+
+  useEffect(() => {
+    const handleRedirect = async () => {
+      if (typeof token === 'string') {
+        await storeToken(token);
+        Alert.alert('Success', 'You are now logged in!');
+        router.replace('/(tabs)/google-profile');
+      } else {
+        Alert.alert('Error', 'No token received');
+        router.replace('/');
+      }
+    };
+
+    handleRedirect();
+  }, []);
+
+  return null;
+}
